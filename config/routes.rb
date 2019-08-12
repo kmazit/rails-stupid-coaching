f065d61fd46f6fc65aa4edb1804619abfd9ecdf6
Karima Mazit <karima_mazit@yahoo.fr>
2019-08-12 15:06:56 +0200

    m

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  #get 'questions/ask'
+  #get 'questions/answer'
+
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+
+  #get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
